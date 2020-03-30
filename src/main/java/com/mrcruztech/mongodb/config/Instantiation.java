b50da64ff59392b316e2cfaf79679771c59c0da2
Matheus Rodrigues da Cruz <matheus.rcruz12@gmail.com>
2020-03-30 16:38:54 -0300

    insert authorDTO in project

--- src/main/java/com/mrcruztech/mongodb/config/Instantiation.java
+++ src/main/java/com/mrcruztech/mongodb/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.mrcruztech.mongodb.config;
 
 import com.mrcruztech.mongodb.domain.Post;
 import com.mrcruztech.mongodb.domain.User;
+import com.mrcruztech.mongodb.dto.AuthorDTO;
 import com.mrcruztech.mongodb.repository.PostRepository;
 import com.mrcruztech.mongodb.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,10 +36,10 @@ public class Instantiation implements CommandLineRunner {
         User wilian = new User(null, "Wilian R da Cruz", "wilian@gmail.com");
         User lucas = new User(null, "Lucas R da Cruz", "lucas@gmail.com");
 
-        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para SJC", ananda);
-        Post post2 = new Post(null, sdf.parse("21/03/2020"), "6 meses", "Hoje fazemos 6 meses de casados!", ananda);
-
         userRepo.saveAll(Arrays.asList(ananda, marco, wilian, lucas));
+
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para SJC", new AuthorDTO(ananda));
+        Post post2 = new Post(null, sdf.parse("21/03/2020"), "6 meses", "Hoje fazemos 6 meses de casados!", new AuthorDTO(ananda));
         postRepo.saveAll(Arrays.asList(post1, post2));
     }
 }
