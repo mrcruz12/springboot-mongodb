9f7f7a008b6cd3a24987f115e32121b7281344d2
Matheus Rodrigues da Cruz <matheus.rcruz12@gmail.com>
2020-03-30 17:28:29 -0300

    add comments in post

--- src/main/java/com/mrcruztech/mongodb/config/Instantiation.java
+++ src/main/java/com/mrcruztech/mongodb/config/Instantiation.java
@@ -3,6 +3,7 @@ package com.mrcruztech.mongodb.config;
 import com.mrcruztech.mongodb.domain.Post;
 import com.mrcruztech.mongodb.domain.User;
 import com.mrcruztech.mongodb.dto.AuthorDTO;
+import com.mrcruztech.mongodb.dto.CommentDTO;
 import com.mrcruztech.mongodb.repository.PostRepository;
 import com.mrcruztech.mongodb.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -40,6 +41,15 @@ public class Instantiation implements CommandLineRunner {
 
         Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para SJC", new AuthorDTO(ananda));
         Post post2 = new Post(null, sdf.parse("21/03/2020"), "6 meses", "Hoje fazemos 6 meses de casados!", new AuthorDTO(ananda));
+
+        CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(wilian));
+
+        CommentDTO c2 = new CommentDTO("Parabens ao casal!", sdf.parse("21/03/2020"), new AuthorDTO(wilian));
+        CommentDTO c3 = new CommentDTO("Felicidades!", sdf.parse("21/03/2020"), new AuthorDTO(marco));
+
+        post1.getComments().addAll(Arrays.asList(c1));
+        post2.getComments().addAll(Arrays.asList(c2, c3));
+
         postRepo.saveAll(Arrays.asList(post1, post2));
 
         ananda.getPosts().addAll(Arrays.asList(post1, post2));
